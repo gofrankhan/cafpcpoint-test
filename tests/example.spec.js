6de6db0aee7c9c8da761ed28b73ac56a806b42f6
Md. Gofran Khan <gofran0123@gmail.com>
2025-08-10 17:29:40 +0600

    initialize cafpcpoint

---
+++ tests/example.spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+test('has title for Caf', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/CAF PC POINT/);
+});
+
+test('get started link for Caf', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/');
+
+  await page.getByPlaceholder('Username').fill('gofran.khan');
+  await page.getByPlaceholder('Password').fill('123');
+  // Click the get started link.
+  await page.getByRole('button', { name: 'Log in' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+});
