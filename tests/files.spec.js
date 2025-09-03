af815c659365d6c3957fd2c42e8ee8f6bd369c7a
Md. Gofran Khan <gofran0123@gmail.com>
2025-09-03 14:26:32 +0600

    delete file test case added

--- tests/files.spec.js
+++ tests/files.spec.js
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 const { createCustomer, createCustomerData, deleteCustomerByTaxId } = require('../steps/customer_steps.js');
 const { saveCustomerData, getCustomerData, } = require('../utils/dataStore');
 
-test('Create a file with new customer and current user', async ({ page }) => {
+test.skip('Create a file with new customer and current user', async ({ page }) => {
     await page.goto('/dashboard');
     // Expects page to have a heading with the name of Installation.
     await page.click('#btn_customer_simple');
@@ -25,5 +25,24 @@ test('Create a file with new customer and current user', async ({ page }) => {
     await expect(page.locator('#last_name')).toHaveValue(customerData.lastname);
     await expect(page.locator('#date_of_birth')).toHaveValue(customerData.dateofbirth);
     await expect(page.locator('#telephone')).toHaveValue(customerData.telephone);
+    await page.getByRole('button', { name: 'Create' }).click();
+    await page.waitForTimeout(3000);
+});
+
+
+test('delete a customer from the top of the table', async ({ page }) => {
+    test.skip(test.info().project.name !== 'super-admin', 'Only valid for admin');
+    await page.goto('/dashboard');
+    await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+    await page.click('#btn_file_simple');
+
+    // Wait for the table to be visible
+    await page.waitForSelector('table');
+
+    const strTaxID = await page.locator('table tr:nth-of-type(1) td:nth-of-type(2)').first().innerText();
+    page.on('dialog', async dialog => { await dialog.accept(); });
+    await page.locator('a.btn.btn-danger.btn-sm.edit[title="Delete"]').first().click();
+    await expect(page.locator(`text=${strTaxID}`)).not.toBeVisible();
+
     await page.waitForTimeout(3000);
 });
